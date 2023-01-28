0f21513bae3be2f955ba29851a0d8e6f94d689d9
Brayden N <gamerinfusion@gmail.com>
2023-01-28 11:32:18 -0500

    Added shuffleboard UI

--- src/main/java/frc/robot/subsystems/Grabber.java
+++ src/main/java/frc/robot/subsystems/Grabber.java
@@ -3,7 +3,10 @@ package frc.robot.subsystems;
 import frc.robot.Constants.GrabberConstants;
 import frc.robot.commands.CloseGrabber;
 
+import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.revrobotics.CANSparkMax;
@@ -12,10 +15,14 @@ public class Grabber extends SubsystemBase {
 	private DigitalInput closedSensor = new DigitalInput(GrabberConstants.closedSensorPin);
 	private CANSparkMax motor = new CANSparkMax(GrabberConstants.motorId, CANSparkMax.MotorType.kBrushless);
 	private DigitalInput objectSensor = new DigitalInput(GrabberConstants.objectSensorPin);
+	private GenericEntry ShuffleboardEntry;
 
 	/** Creates a new Grabber. */
 	public Grabber() {
 		motor.set(0);
+		ShuffleboardEntry = Shuffleboard.getTab("Drive").add("Is Closed?", false)
+			.withWidget(BuiltInWidgets.kBooleanBox)
+			.getEntry(); // Get an entry widget
 	}
 
 	public boolean objectDetected() {
@@ -45,5 +52,6 @@ public class Grabber extends SubsystemBase {
 		if (objectDetected() && !closeGrabber.isScheduled()) {
 			closeGrabber.schedule();
 		}
+		ShuffleboardEntry.setBoolean(isClosed());
 	}
 }
