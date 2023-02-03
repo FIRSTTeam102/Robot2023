eb8de98913288f1736da65f1e3806b6cf770c4de
Seth <swcaskey@gmail.com>
2023-02-02 20:06:55 -0500

    networktablesapriltags

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -18,16 +18,42 @@ public class Vision extends SubsystemBase {
 	@Override
 	public void periodic() {
 
-		double x = tx.getDouble(0.0);
-		double y = ty.getDouble(0.0);
-		double area = ta.getDouble(0.0);
+		double v = tv.getDouble(0.0);
+		double tx = tx.getDouble(0.0);
+		double ty = ty.getDouble(0.0);
+		double ta = ta.getDouble(0.0);
+		double APID = ID.getDoubleArray(double[1]);
+		double TX = TX.getDoubleArray(double[1]);
+		double TY = TX.getDoubleArray(double[2]);
+		double TZ = TX.getDoubleArray(double[3]);
+		double RX = TX.getDoubleArray(double[4]);
+		double RY = TX.getDoubleArray(double[5]);
+		double RZ = TX.getDoubleArray(double[6]);
+		double tclass = tclass.getDouble(0.0);
 
-		SmartDashboard.putNumber("LimelightX", x);
-		SmartDashboard.putNumber("LimelightY", y);
-		SmartDashboard.putNumber("LimelightArea", area);
+		SmartDashboard.putNumber("Limelight tv", v);
+		SmartDashboard.putNumber("Limelight tx", tx);
+		SmartDashboard.putNumber("Limelight ty", ty);
+		SmartDashboard.putNumber("Limelight ta", ta);
+		SmartDashboard.putNumber("Limelight APID", APID);
+		SmartDashboard.putNumber("Limelight TX", TX);
+		SmartDashboard.putNumber("Limelight TY", TY);
+		SmartDashboard.putNumber("Limelight TZ", TZ);
+		SmartDashboard.putNumber("Limelight RX", RX);
+		SmartDashboard.putNumber("Limelight RY", RY);
+		SmartDashboard.putNumber("Limelight RZ", RZ);
+		SmartDashboard.putNumber("Limelight tclass", tclass);
 
-		System.out.println(x);
-		System.out.println(y);
-		System.out.println(area);
+		System.out.println(v);
+		System.out.println(tx);
+		System.out.println(ty);
+		System.out.println(ta);
+		System.out.println(TX);
+		System.out.println(TY);
+		System.out.println(TZ);
+		System.out.println(RX);
+		System.out.println(RY);
+		System.out.println(RZ);
+		System.out.println(tclass);
 	}
 }
